57644b7369ab2487015a4979555b96189f6a6a39
Arya <aryapanwar493@gmail.com>
2025-03-06 11:45:41 +0530

    minor changes

--- ai-tutor/src/api/server.py
+++ ai-tutor/src/api/server.py
@@ -1,13 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import google.generativeai as genai
 import fitz
 import yt_dlp
 import threading
 import os
 from gemini-app import generate_content, search_youtube, extract_text_from_pdf
-
-genai.configure(api_key="AIzaSyCKeI0ntu6btu3Wybvpsjk3lvnInso5LG0")
 
 app = Flask(__name__)
 CORS(app)
